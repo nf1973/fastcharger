a65f76556fbcc4e308febd8ec187684deaae0f65
nf1973 <56644716+nf1973@users.noreply.github.com>
2023-12-20 14:23:11 +0100

    vercel api

--- next.config.js
+++ next.config.js
@@ -15,20 +15,3 @@ module.exports = {
     ];
   },
 };
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         destination:
-//           process.env.NODE_ENV === "development"
-//             ? "http://127.0.0.1:5328/api/:path*"
-//             : "/api/",
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = nextConfig;
